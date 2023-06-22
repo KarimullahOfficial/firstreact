83306e8695f73137c3a73163d593b016b1998832
Karimullah <59876062+KarimullahOfficial@users.noreply.github.com>
2023-06-22 11:51:52 +0500

    p

--- my/src/App.jsx
+++ my/src/App.jsx
@@ -1,19 +1,9 @@
 import "./App.css";
+import Navigation from "./component/Navigation";
 const App = () => {
   return (
     <div>
-      <nav>
-        <div className="log">
-          <img src="../public/images/brand_logo.png" alt="logo"></img>
-        </div>
-        <ul>
-          <li href="#">Menu</li>
-          <li href="#">Location</li>
-          <li href="#">About </li>
-          <li href="">Contact</li>
-        </ul>
-        <button>Login</button>
-      </nav>
+      <Navigation></Navigation>
     </div>
   );
 };
---
+++ my/src/component/Navigation.jsx
@@ -0,0 +1,17 @@
+const Navigation = () => {
+  return (
+    <nav>
+      <div className="log">
+        <img src="../public/images/brand_logo.png" alt="logo"></img>
+      </div>
+      <ul>
+        <li href="#">Menu</li>
+        <li href="#">Location</li>
+        <li href="#">About </li>
+        <li href="">Contact</li>
+      </ul>
+      <button>Login</button>
+    </nav>
+  );
+};
+export default Navigation;
